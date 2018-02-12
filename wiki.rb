4bd3799b5925d286042d6c54bc70bd402e318f3d
shirin <shirin@oa-edu-178-53.wireless.abdn.ac.uk>
2018-02-12 12:05:50 +0000

    User edit accessible only by admin

--- wiki.rb
+++ wiki.rb
@@ -2,13 +2,13 @@ require 'sinatra'
 require 'sinatra/activerecord'
 
 ActiveRecord::Base.establish_connection(
-	:adapter => 'sqlite3',
-	:database => 'wiki.db'
-)
+  :adapter => 'sqlite3',
+  :database => 'wiki.db'
+  )
 
 class User < ActiveRecord::Base
-	validates :username, presence: true, uniqueness: true
-	validates :password, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
 end
 
 $myinfo = "Aleksandra Chybowska"
@@ -16,158 +16,167 @@ $myinfo = "Aleksandra Chybowska"
 $credentials = ['','']
 
 def readFile(filename)
-	info = ""
-	file = File.open(filename)
-	
-	file.each do |line|
-		info = info + line
-	end
+  info = ""
+  file = File.open(filename)
+  
+  file.each do |line|
+    info = info + line
+  end
 
-	file.close
-	return info
+  file.close
+  return info
 end
 
 
 def authorized?
-    if $credentials != nil
+  if $credentials != nil
     @Userz = User.where(:username => $credentials[0]).to_a.first
-        if @Userz
-                if @Userz.edit == true
-                    return true
-                else
-                    return false
-                end
-        else
-         return false
-        end
-    end
+    if @Userz
+      if @Userz.edit == true
+        return true
+      else
+        return false
+      end
+    else
+     return false
+   end
+ end
 end
 
 def protected!
-    if authorized?
-        return
-    end
-    redirect '/denied'
+  if authorized?
+    return
+  end
+  redirect '/denied'
 end
-    
+
+def protected_admin!
+  if $credentials != nil && $credentials[0].to_s == 'Admin'
+    return
+  end
+  redirect '/denied'
+end
+
 def reverse (string)
-     string.each_char.to_a.reverse.join
+ string.each_char.to_a.reverse.join
 end
 
 
 get '/' do
-	@info  = readFile("wiki.txt").chomp
-	len = @info.length 
-	@words = len.to_s
+  @info  = readFile("wiki.txt").chomp
+  len = @info.length 
+  @words = len.to_s
 
-	erb :home
+  erb :home
 end
 
 get '/about' do
-	erb :about
+  erb :about
 end
 
 get '/register' do
-	erb :register
+  erb :register
 end
 
 post '/register' do
-    n = User.new   
-    n.username = params[:username]
-    n.password = params[:password]    
-    
-    if n.username == "Admin" and n.password == "Password"
-   		n.edit = true 
-    end
-   	
-   	n.save    
-   	redirect "/"
+  n = User.new   
+  n.username = params[:username]
+  n.password = params[:password]    
+
+  if n.username == "Admin" and n.password == "Password"
+    n.edit = true 
+  end
+
+  n.save    
+  redirect "/"
 end
 
 get '/logout' do
-    $credentials = ['','']
-    redirect '/'
-    end
+  $credentials = ['','']
+  redirect '/'
+end
 
 get '/login' do
-	erb :login
+  erb :login
 end
 
 
 post '/login' do
-    $credentials = [params[:username],params[:password]]
-    @Users = User.where(:username => $credentials[0]).to_a.first
-    
-    if @Users && @Users.password == $credentials[1]
-        redirect '/'
-    else
-        $credentials = ['','']
-        redirect '/wrongaccount'
-    end
+  $credentials = [params[:username],params[:password]]
+  @Users = User.where(:username => $credentials[0]).to_a.first
+
+  if @Users && @Users.password == $credentials[1]
+    redirect '/'
+  else
+    $credentials = ['','']
+    redirect '/wrongaccount'
+  end
 end
-    
-    
+
+
 get '/wrongaccount' do
-    erb :wrongaccount
+  erb :wrongaccount
 end
 
 get '/edit' do
-    protected!
-    info =""
-    file=File.open("wiki.txt")
-    file.each do |line|
- end
- file.close
- @info=info
- erb :edit
- end
+  protected!
+  info =""
+  file=File.open("wiki.txt")
+  file.each do |line|
+    info = info + line
+  end
+
+  file.close
+  @info=info
+  erb :edit
+end
 
 
 put '/edit' do
-	info = "#{params[:message]}"
-	@info = info
-	file = File.open("wiki.txt", "w")
-	file.puts @info
-	file.close
-	redirect '/'
+  info = "#{params[:message]}"
+  @info = info
+  file = File.open("wiki.txt", "w")
+  file.puts @info
+  file.close
+  redirect '/'
 end
 
 get '/admincontrols' do
-    protected!
-    @list2 = User.all.sort_by { |u| [u.id] }
-    erb :admincontrols
-    end
+  protected_admin!
+  @list2 = User.all.sort_by { |u| [u.id] }
+  erb :admincontrols
+end
 
 get '/denied' do
-    erb :denied
-    end
+  erb :denied
+end
 
 put '/user/:uzer' do
-    protected!
-    n = User.where(:username => params[:uzer]).to_a.first
-    n.edit = params[:edit] ? 1 : 0
-    n.save
-    redirect '/'
-    end
+  protected_admin!
+  n = User.where(:username => params[:uzer]).to_a.first
+  n.edit = params[:edit] ? 1 : 0
+  n.save
+  redirect '/'
+end
 
 get '/user/delete/:uzer' do
-    protected!
-    n = User.where(:username => params[:uzer]).to_a.first
-    if n.username == "Admin"
-        erb :denied
-    else
-        n.destroy
-        redirect '/admincontrols'
-    end
+  protected_admin!
+  n = User.where(:username => params[:uzer]).to_a.first
+  if n.username == "Admin"
+    erb :denied
+  else
+    n.destroy
+    redirect '/admincontrols'
   end
+end
 
-    
+
 get '/reverse' do
-    $myinfo = reverse($myinfo)
-    redirect '/'
+  $myinfo = reverse($myinfo)
+  redirect '/'
 end
 
 not_found do 
-	status 404
-	erb :notfound
+  status 404
+  erb :notfound
 end
